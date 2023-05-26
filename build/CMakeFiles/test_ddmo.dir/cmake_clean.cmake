e9b2ca3b97057dcbae8b6d4482ce25ee6e076176
JackZhang9 <843946042@qq.com>
2023-05-26 13:59:10 +0800

    first

---
+++ build/CMakeFiles/test_ddmo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_ddmo.dir/main.cpp.o"
+  "../test_ddmo.pdb"
+  "../test_ddmo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_ddmo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
